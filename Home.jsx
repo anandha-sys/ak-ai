9347410f986cbaef4abbe894a804149f3c644f4e
anandha-sys <krishnanhanandha45@gmail.com>
2026-06-26 12:55:08 +0530

    Add Home component with ChatBox integration

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import ChatBox from "../components/ChatBox";
+
+function Home() {
+  return (
+    <div>
+      <h1>AK AI Assistant</h1>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default Home;
